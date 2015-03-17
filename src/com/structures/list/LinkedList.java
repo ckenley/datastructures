a3a6ee6878cd38e4b008c785beffc59dbe2fb3e0
CRK <crkenley@gmail.com>
2015-03-17 11:39:20 -0500

    Updated LinkedList, LinkedListIterator implementations, and updated relevant tests.

--- src/com/structures/list/LinkedList.java
+++ src/com/structures/list/LinkedList.java
@@ -3,171 +3,185 @@ package com.structures.list;
 import com.structures.node.ListNode;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /*
  * A singly-linked LinkedList<T> implementation of List<T>
  */
 public class LinkedList<T> implements List<T>, Iterable<T> {
 
-    private ListNode<T> head;
-    private int size;
+	private ListNode<T> head;
+	private int size;
 
-    public LinkedList() {
-        this.head = new ListNode<T>();
-        this.size = 0;
-    }
+	public LinkedList() {
+		this.head = null;
+		this.size = 0;
+	}
 
-    public LinkedList(ListNode<T> head) {
-        this.head = head;
-        this.size = 0;
-    }
+	public LinkedList(ListNode<T> head) {
+		this.head = head;
+		this.size = 0;
+	}
 
-    public LinkedList(int size) {
-        for (int i = 0; i < size; i++) {
-            add(null);
-        }
-        this.size = size;
-    }
+	public LinkedList(int size) {
+		for (int i = 0; i < size; i++) {
+			add(null);
+		}
+		this.size = size;
+	}
 
-    public ListNode<T> getHead() {
-        return head;
-    }
+	public ListNode<T> getHead() {
+		return head;
+	}
 
-    public void setHead(ListNode<T> head) {
-        this.head = head;
-    }
+	public void setHead(ListNode<T> head) {
+		this.head = head;
+	}
 
-    @Override
-    public int size() {
-        return size;
-    }
+	@Override
+	public int size() {
+		return size;
+	}
 
-    @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            private int index;
+	@Override
+	public Iterator<T> iterator() {
+		return new LinkedListIterator(this);
+	}
 
-            @Override
-            public boolean hasNext() {
-                return this.index < size();
-            }
+	@Override
+	public boolean add(T value) {
+		if (head == null) {
+			head = new ListNode<T>(value);
+			size++;
+			return true;
+		} else {
+			ListNode<T> n = head;
+			while (n.getNext() != null) {
+				n = n.getNext();
+			}
+			n.setNext(new ListNode<T>(value));
+			size++;
+			return true;
+		}
+	}
 
-            @Override
-            public T next() {
-                return get(this.index++);
-            }
-        };
-    }
+	@Override
+	public void clear() {
+		head = null;
+		size = 0;
+	}
 
-    @Override
-    public boolean add(T value) {
-        if (head == null) {
-            head = new ListNode<T>(value);
-            size++;
-            return true;
-        } else {
-            ListNode<T> n = head;
-            while (n.getNext() != null) {
-                n = n.getNext();
-            }
-            n.setNext(new ListNode<T>(value));
-            size++;
-            return true;
-        }
-    }
+	@Override
+	public T set(int index, T value) {
+		if (isEmpty() || index < 0 || index >= size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		int i = 0;
+		ListNode<T> n = head;
+		while (i < index && n.getNext() != null) {
+			n = n.getNext();
+			i++;
+		}
+		n.setValue(value);
+		return value;
+	}
 
-    @Override
-    public void clear() {
-        head = null;
-        size = 0;
-    }
+	@Override
+	public T get(int index) {
+		if (isEmpty() || index < 0 || index >= size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		int i = 0;
+		ListNode<T> n = head;
+		while (i < index && n.getNext() != null) {
+			n = n.getNext();
+			i++;
+		}
+		return n.getValue();
+	}
 
-    @Override
-    public T set(int index, T value) {
-        if (isEmpty() || index < 0 || index >= size()) {
-            throw new IndexOutOfBoundsException();
-        }
-        int i = 0;
-        ListNode<T> n = head;
-        while (i < index && n.getNext() != null) {
-            n = n.getNext();
-            i++;
-        }
-        n.setValue(value);
-        return value;
-    }
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
-    @Override
-    public T get(int index) {
-        if (isEmpty() || index < 0 || index >= size()) {
-            throw new IndexOutOfBoundsException();
-        }
-        int i = 0;
-        ListNode<T> n = head;
-        while (i < index && n.getNext() != null) {
-            n = n.getNext();
-            i++;
-        }
-        return n.getValue();
-    }
+	@Override
+	public boolean remove(int index) {
+		if (isEmpty() || index < 0 || index > size() - 1) {
+			return false;
+		}
+		ListNode<T> n = head;
+		for (int i = 0; i < index; i++) {
+			n = n.getNext();
+		}
+		n = null;
+		size--;
+		return true;
+	}
 
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
+	@Override
+	public int indexOf(T value) {
+		if (this.size() > 0) {
+			ListNode<T> n = head;
+			for (int i = 1; i <= size(); i++) {
+				if (n.getValue().equals(value)) {
+					return i;
+				}
+				n = n.getNext();
+			}
+		}
+		return -1;
+	}
 
-    @Override
-    public boolean remove(int index) {
-        if (isEmpty() || index < 0 || index > size() - 1) {
-            return false;
-        }
-        ListNode<T> n = head;
-        for (int i = 0; i < index; i++) {
-            n = n.getNext();
-        }
-        n = null;
-        size--;
-        return true;
-    }
+	public void addFirst(T value) {
+		if (head == null) {
+			head.setValue(value);
+		} else {
+			ListNode<T> prev = head;
+			head = new ListNode<T>(value);
+			head.setNext(prev);
+		}
+		size++;
+	}
 
-    @Override
-    public int indexOf(T value) {
-        if (this.size() > 0) {
-            ListNode n = head;
-            for (int i = 1; i <= size(); i++) {
-                if (n.getValue().equals(value)) {
-                    return i;
-                }
-                n = n.getNext();
-            }
-        }
-        return -1;
-    }
+	public void addLast(T value) {
+		if (value != null) {
+			if (head == null) {
+				head.setValue(value);
+			} else {
+				ListNode<T> n = head;
+				while (n.getNext() != null) {
+					n = n.getNext();
+				}
+				n.setNext(new ListNode<T>(value));
+			}
+			size++;
+		}
+	}
 
-    public void addFirst(T value) {
-        if (head == null) {
-            head.setValue(value);
-        } else {
-            ListNode<T> prev = head;
-            head = new ListNode<T>(value);
-            head.setNext(prev);
-        }
-        size++;
-    }
+	private class LinkedListIterator implements Iterator<T> {
 
-    public void addLast(T value) {
-        if (value != null) {
-            if (head == null) {
-                head.setValue(value);
-            } else {
-                ListNode<T> n = head;
-                while (n.getNext() != null) {
-                    n = n.getNext();
-                }
-                n.setNext(new ListNode<T>(value));
-            }
-            size++;
-        }
-    }
+		private LinkedList<T> linkedList;
+		private int index;
+
+		LinkedListIterator(LinkedList<T> linkedList) {
+			this.linkedList = linkedList;
+			this.index = 0;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return index < linkedList.size;
+		}
+
+		@Override
+		public T next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			return linkedList.get(index++);
+		}
+
+	}
 
 }
--- src/com/structures/test/LinkedListTest.java
+++ src/com/structures/test/LinkedListTest.java
@@ -1,140 +1,141 @@
 package com.structures.test;
 
-import com.structures.list.LinkedList;
+import java.util.Iterator;
+
 import junit.framework.TestCase;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import java.util.Iterator;
+import com.structures.list.LinkedList;
 
 public class LinkedListTest extends TestCase {
 
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 
-    private LinkedList<String> testList;
+	private LinkedList<String> testList;
 
-    @Before
-    protected void setUp() {
-        testList = new LinkedList<String>();
-    }
+	@Before
+	protected void setUp() {
+		testList = new LinkedList<String>();
+	}
 
-    @After
-    protected void tearDown() {
-        testList = null;
-    }
+	@After
+	protected void tearDown() {
+		testList = null;
+	}
 
-    @Test
-    public void testAdd() {
-        testList.add("value1");
-        assertEquals("value1", testList.get(0));
-    }
+	@Test
+	public void testAdd() {
+		testList.add("value1");
+		assertEquals("value1", testList.get(0));
+	}
 
-    @Test
-    public void testSize() {
-        testList.add("value1");
-        assertEquals(testList.size(), 1);
-        testList.add("value2");
-        assertEquals(testList.size(), 2);
-        testList.add("value3");
-        assertEquals(testList.size(), 3);
-        assertFalse(testList.size() == 4);
-    }
+	@Test
+	public void testSize() {
+		testList.add("value1");
+		assertEquals(1, testList.size());
+		testList.add("value2");
+		assertEquals(2, testList.size());
+		testList.add("value3");
+		assertEquals(3, testList.size());
+		assertFalse(testList.size() == 4);
+	}
 
-    @Test
-    public void testClear() {
-        testList.add("new string");
-        assertFalse(testList.isEmpty());
-        testList.clear();
-        assertTrue(testList.isEmpty());
-    }
+	@Test
+	public void testClear() {
+		testList.add("new string");
+		assertFalse(testList.isEmpty());
+		testList.clear();
+		assertTrue(testList.isEmpty());
+	}
 
-    @Test
-    public void testGet() {
-        testList.add("1");
-        testList.add("2");
-        testList.add("3");
-        assertEquals("1", testList.get(0));
-        assertEquals("2", testList.get(1));
+	@Test
+	public void testGet() {
+		testList.add("1");
+		testList.add("2");
+		testList.add("3");
+		assertEquals("1", testList.get(0));
+		assertEquals("2", testList.get(1));
 
-        exception.expect(IndexOutOfBoundsException.class);
-        assertEquals("3", testList.get(2));
-    }
+		exception.expect(IndexOutOfBoundsException.class);
+		assertEquals("3", testList.get(2));
+	}
 
-    @Test
-    public void testIsEmpty() {
-        assertTrue(testList.isEmpty());
-        testList.add("1");
-        assertFalse(testList.isEmpty());
-    }
+	@Test
+	public void testIsEmpty() {
+		assertTrue(testList.isEmpty());
+		testList.add("1");
+		assertFalse(testList.isEmpty());
+	}
 
-    @Test
-    public void testRemove() {
-        testList.add("1");
-        assertFalse(testList.remove(1));
-        assertFalse(testList.remove(-1));
-        assertTrue(testList.remove(0));
-    }
+	@Test
+	public void testRemove() {
+		testList.add("1");
+		assertFalse(testList.remove(1));
+		assertFalse(testList.remove(-1));
+		assertTrue(testList.remove(0));
+	}
 
-    @Test
-    public void testIndexOf() {
-        testList.add("1");
-        testList.add("2");
-        testList.add("3");
-        assertEquals(testList.indexOf("1"), 1);
-        assertEquals(testList.indexOf("2"), 2);
-        assertEquals(testList.indexOf("3"), 3);
-        assertEquals(testList.indexOf("99"), -1);
-    }
+	@Test
+	public void testIndexOf() {
+		testList.add("1");
+		testList.add("2");
+		testList.add("3");
+		assertEquals(1, testList.indexOf("1"));
+		assertEquals(2, testList.indexOf("2"));
+		assertEquals(3, testList.indexOf("3"));
+		assertEquals(-1, testList.indexOf("99"));
+	}
 
-    @Test
-    public void testAddFirst() {
-        testList.add("1");
-        testList.add("2");
-        testList.addFirst("3");
-        assertFalse(testList.get(0).equals("1"));
-        assertTrue(testList.get(0).equals("3"));
-        assertTrue(testList.get(1).equals("1"));
-        assertTrue(testList.get(2).equals("2"));
+	@Test
+	public void testAddFirst() {
+		testList.add("1");
+		testList.add("2");
+		testList.addFirst("3");
+		assertFalse(testList.get(0).equals("1"));
+		assertTrue(testList.get(0).equals("3"));
+		assertTrue(testList.get(1).equals("1"));
+		assertTrue(testList.get(2).equals("2"));
 
-    }
+	}
 
-    @Test
-    public void testAddLast() {
-        testList.add("1");
-        testList.add("2");
-        testList.addLast("9");
-        assertTrue(testList.get(0).equals("1"));
-        assertFalse(testList.get(0).equals("3"));
-        assertTrue(testList.get(2).equals("9"));
-    }
+	@Test
+	public void testAddLast() {
+		testList.add("1");
+		testList.add("2");
+		testList.addLast("9");
+		assertEquals("1", testList.get(0));
+		assertFalse(testList.get(0).equals("3"));
+		assertEquals("9", testList.get(2));
+	}
 
-    @Test
-    public void testSet() {
-        testList.add("1");
-        testList.add("2");
-        testList.add("3");
-        assertEquals(testList.get(1), "2");
-        assertEquals(testList.set(1, "3"), "3");
-        assertEquals(testList.get(1), "3");
-    }
+	@Test
+	public void testSet() {
+		testList.add("1");
+		testList.add("2");
+		testList.add("3");
+		assertEquals("2", testList.get(1));
+		assertEquals("3", testList.set(1, "3"));
+		assertEquals("3", testList.get(1));
+	}
 
-
-    @Test
-    public void testIterator() {
-        testList.add("1");
-        testList.add("2");
-        testList.add("3");
-        assert (testList.iterator() != null);
-        assert (testList.iterator() instanceof Iterator);
-        Iterator<String> iterator = testList.iterator();
-        assert (iterator.next().equals("1"));
-        assert (iterator.next().equals("2"));
-        assert (iterator.next().equals("3"));
-        assertFalse(iterator.hasNext());
-    }
+	@Test
+	public void testIterator() {
+		testList.add("1");
+		testList.add("2");
+		testList.add("3");
+		Iterator<String> iterator = testList.iterator();
+		assert (iterator != null);
+		assert (iterator instanceof Iterator);
+		assertEquals("1", iterator.next());
+		assertEquals("2", iterator.next());
+		assertEquals("3", iterator.next());
+		assertFalse(iterator.hasNext());
+	}
 
 }
