3a04d7ec4bbe9785718cfc7aec63cf33aec27436
CRK <crkenley@gmail.com>
2015-03-20 13:56:27 -0500

    Revert "Implementing more HashMap methods, LinkedList.remove(int index) methed, and associated tests."
    This reverts commit c0a5684c45d43720afb4318cc8cc6515af529ea1.

--- src/com/structures/test/HashMapTest.java
+++ src/com/structures/test/HashMapTest.java
@@ -11,96 +11,89 @@ import com.structures.map.Map;
 
 public class HashMapTest {
 
-	private final int SIZE = 128;
-	private Map<String, String> testMap;
+    private final int SIZE = 128;
+    private Map<String, String> testMap;
 
-	@Before
-	public void setUp() {
-		testMap = new HashMap<String, String>(SIZE);
-	}
+    @Before
+    public void setUp() {
+        testMap = new HashMap<String, String>(SIZE);
+    }
 
-	@After
-	public void tearDown() {
-		testMap = null;
-	}
+    @After
+    public void tearDown() {
+        testMap = null;
+    }
+    
+    @Test
+    public void testContructor(){
+    	assertTrue(testMap.size() == 128);
+    }
 
-	@Test
-	public void testContructor() {
-		assertTrue(testMap.size() == 128);
-	}
+    @Test
+    public void testClear() throws Exception {
+        assertEquals("Z", testMap.put("A", "Z"));
+        assertTrue(!testMap.isEmpty());
+        testMap.clear();
+        assertTrue(testMap.isEmpty());
+    }
 
-	@Test
-	public void testClear() throws Exception {
-		assertEquals("Z", testMap.put("A", "Z"));
-		assertTrue(!testMap.isEmpty());
-		testMap.clear();
-		assertTrue(testMap.isEmpty());
-	}
+    @Test
+    public void testContainsKey() throws Exception {
+    	testMap.put("A", "Z");
+    	testMap.put("Z", "A");
+    	assertTrue(testMap.containsKey("A"));
+    	assertFalse(testMap.containsKey("C"));
+    	assertTrue(testMap.containsKey("Z"));
+    }
 
-	@Test
-	public void testContainsKey() throws Exception {
-		testMap.put("A", "Z");
-		testMap.put("Z", "A");
-		assertTrue(testMap.containsKey("A"));
-		assertFalse(testMap.containsKey("C"));
-		assertTrue(testMap.containsKey("Z"));
-	}
+    @Test
+    public void testContainsValue() throws Exception {
+    	testMap.put("A", "Z");
+    	testMap.put("Z", "A");
+    	assertTrue(testMap.containsValue("A"));
+    	assertTrue(testMap.containsValue("Z"));
+    	assertFalse(testMap.containsValue("C"));
+    }
 
-	@Test
-	public void testContainsValue() throws Exception {
-		testMap.put("A", "Z");
-		testMap.put("Z", "A");
-		assertTrue(testMap.containsValue("A"));
-		assertTrue(testMap.containsValue("Z"));
-		assertFalse(testMap.containsValue("C"));
-	}
+    @Test
+    public void testEntrySet() throws Exception {
 
-	@Test
-	public void testEntrySet() throws Exception {
+    }
 
-	}
+    @Test
+    public void testGet() throws Exception {
 
-	@Test
-	public void testGet() throws Exception {
+    }
 
-	}
+    @Test
+    public void testIsEmpty() throws Exception {
+    	assertTrue(testMap.isEmpty());
+    	testMap.put("A", "Z");
+    	assertFalse(testMap.isEmpty());
+    }
 
-	@Test
-	public void testIsEmpty() throws Exception {
-		assertTrue(testMap.isEmpty());
-		testMap.put("A", "Z");
-		assertFalse(testMap.isEmpty());
-	}
+    @Test
+    public void testKeySet() throws Exception {
 
-	@Test
-	public void testKeySet() throws Exception {
+    }
 
-	}
+    @Test
+    public void testPut() throws Exception {
 
-	@Test
-	public void testPut() throws Exception {
+    }
 
-	}
+    @Test
+    public void testRemove() throws Exception {
 
-	@Test
-	public void testRemove() throws Exception {
-		testMap.put("A", "Z");
-		testMap.put("B", "Y");
-		testMap.put("C", "X");
-		assertEquals("Y", testMap.remove("B"));
-		assertFalse(testMap.containsKey("B"));
-		assertFalse(testMap.containsValue("Y"));
-		assertTrue(testMap.containsKey("A"));
-		assertTrue(testMap.containsKey("C"));
-	}
+    }
 
-	@Test
-	public void testSize() throws Exception {
+    @Test
+    public void testSize() throws Exception {
 
-	}
+    }
 
-	@Test
-	public void testValues() throws Exception {
+    @Test
+    public void testValues() throws Exception {
 
-	}
+    }
 }
--- src/com/structures/test/LinkedListTest.java
+++ src/com/structures/test/LinkedListTest.java
@@ -75,13 +75,9 @@ public class LinkedListTest {
 	@Test
 	public void testRemove() {
 		testList.add("1");
-		testList.add("2");
-		testList.add("3");
-		assertFalse(testList.remove(4));
+		assertFalse(testList.remove(1));
 		assertFalse(testList.remove(-1));
-		assertTrue(testList.remove(2));
 		assertTrue(testList.remove(0));
-		assertEquals("2", testList.get(0));
 	}
 
 	@Test
