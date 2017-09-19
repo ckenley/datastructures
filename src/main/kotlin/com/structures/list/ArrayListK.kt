2eb4ebd2aae0759cb8a3b64ed685cca4befb064e
ckenley <crkenley@gmail.com>
2017-09-18 21:03:21 -0500

    More arraylist implementation
    Signed-off-by: ckenley <crkenley@gmail.com>

--- src/main/kotlin/com/structures/list/ArrayListK.kt
+++ src/main/kotlin/com/structures/list/ArrayListK.kt
@@ -1,54 +1,83 @@
 package com.structures.list
 
-class ArrayListK<E> : kotlin.collections.List<E> {
-
-    override val size: Int
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
-
-    override fun contains(element: E): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun containsAll(elements: Collection<E>): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun get(index: Int): E {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun indexOf(element: E): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun isEmpty(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun iterator(): Iterator<E> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun lastIndexOf(element: E): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun listIterator(): ListIterator<E> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun listIterator(index: Int): ListIterator<E> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun subList(fromIndex: Int, toIndex: Int): List<E> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+class ArrayListK<E> : List<E> {
 
     private val INITIAL_SIZE: Int = 10
     private var currentSize: Int = INITIAL_SIZE
     private var array = arrayOfNulls<Any>(INITIAL_SIZE)
 
+    override fun size(): Int {
+        return currentSize
+    }
 
+    override fun add(value: E): Boolean {
+        if (array.contains(value)) {
+            return false
+        }
+        checkSize()
+        array[currentSize++] = value
+        return true
+    }
+
+    override fun clear() {
+        array = arrayOfNulls(currentSize)
+    }
+
+    override fun remove(index: Int): E {
+        val item: E = get(index)
+        System.arraycopy(array, index + 1, array, index, currentSize--)
+        return item
+    }
+
+    override fun remove(value: E): Boolean {
+        if (value in array) {
+            System.arraycopy(array, array.indexOf(value) + 1, array, array.indexOf(value), currentSize--)
+            return true
+        }
+        return false
+    }
+
+    override fun set(index: Int, value: E): E {
+        val existing = get(index)
+        array[index] = value
+        return existing
+    }
+
+
+    override fun contains(element: E): Boolean {
+        return element in array
+    }
+
+    override fun get(index: Int): E {
+        return array[index] as E
+    }
+
+    /**
+     * Returns first index of [element], or -1 if the collection does not contain element.
+     */
+    override fun indexOf(element: E): Int {
+        if (element in array) {
+            return array.indexOf(element)
+        } else {
+            return -1
+        }
+    }
+
+    override fun isEmpty(): Boolean {
+        return currentSize == 0
+    }
+
+    override fun iterator(): MutableIterator<E> {
+        return array.iterator() as MutableIterator<E>
+    }
+
+    private fun resize() {
+        this.array = array.copyOf(currentSize * 2)
+    }
+
+    private fun checkSize() {
+        if (currentSize >= array.size) {
+            resize()
+        }
+    }
 }
